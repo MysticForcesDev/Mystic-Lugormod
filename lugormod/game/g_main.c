b0ccd29ed9d4af97c4b39c186e60e285b01a1073
Slash <slash@noreply.local>
2024-01-04 12:40:10 +0100

    Bumped version number to 1.0.3

--- lugormod/game/g_main.c
+++ lugormod/game/g_main.c
@@ -20,7 +20,7 @@ const int verBuild = 91;
 #else
 const int verMajor = 1;
 const int verMinor = 0;
-const int verRev = 2;
+const int verRev = 3;
 const int verBuild = 0;
 #endif
 
@@ -44,7 +44,7 @@ const char *verMods =
 
 #else
 
-#define LUGORMODVERSION_CORE "1.0.2"
+#define LUGORMODVERSION_CORE "1.0.3"
 #ifdef LMD_EXPERIMENTAL
 #define LUGORMODVERSION LUGORMODVERSION_CORE" Alpha"
 #else
